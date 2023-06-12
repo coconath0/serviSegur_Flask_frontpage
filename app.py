c589875ea6fcade1211bcbdeb1eee6d6533b1764
coconath0 <naingol10@gmail.com>
2023-06-11 22:49:27 -0500

    just started

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
 
-app= Flask(__name__)
-
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Hello world!!'
+   return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
