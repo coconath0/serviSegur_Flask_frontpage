6d1bf57feaa41a64c1c27985fe19bde705032b90
coconath0 <naingol10@gmail.com>
2023-06-12 23:02:09 -0500

    included rou to json

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 
 app = Flask(__name__, template_folder='template', static_folder='static')
+
+@app.route('/static/particlesjs-config.json')
+def particles_config():
+    with open('static/particlesjs-config.json') as f:
+        config = f.read()
+    return jsonify(config)
 
 @app.route("/")
 def home():
